63697b12ec4028d9a8f4ad0a3b01b004bf2b2bd3
Gabriel Mafra <Gabriel@MacBook-Pro.local>
2017-11-01 11:44:59 -0200

    GET elbs function added

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 #!flask/bin/python
-from flask import Flask, jsonify, make_response
+from flask import Flask, jsonify, make_response, request
 import boto3
 
 app = Flask(__name__)
@@ -12,15 +12,15 @@ def not_found(error):
     return make_response(jsonify({'error': 'The ELB does not exist'}), 404)
 
 @app.errorhandler(400)
-def not_found(error):
+def wrong_data(error):
     return make_response(jsonify({'error': 'Wrong data format'}), 400)
 
 @app.errorhandler(409)
-def not_found(error):
+def instance_attached(error):
     return make_response(jsonify({'error': 'Instance is already on LoadBalancer'}), 409)
 
 @app.errorhandler(408)
-def not_found(error):
+def instance_notattached(error):
     return make_response(jsonify({'error': 'Instance is not on LoadBalacer'}), 408)
 
 @app.route('/healthcheck', methods=['GET'])
@@ -29,9 +29,35 @@ def get_health():
 
 
 
-#@app.route('/elb/<str:elb_name>', methods=['GET'])
-#def get_elb(elb_name):
-#   elb = 
+@app.route('/elb/<elb_name>', methods=['GET'])
+def get_elb(elb_name):
+    assert elb_name == request.view_args['elb_name']
+    elbs =  elb_client.describe_load_balancers(
+    LoadBalancerNames=[
+      elb_name
+    ]
+  )['LoadBalancerDescriptions']
+
+    elb_instances_ids = sum(list(map(lambda elb: list(map(lambda i: i['InstanceId'], elb['Instances'])), elbs)), [])
+
+    reservations = ec2_client.describe_instances(
+        InstanceIds=elb_instances_ids
+    )['Reservations']
+
+    all_instances = []
+
+    for reservation in reservations:
+        for instance in reservation['Instances']:
+            all_instances.append(
+                {
+                    'instanceId': instance['InstanceId'],
+                    'instanceType': instance['InstanceType'],
+                    'launchDate': instance['LaunchTime'].strftime('%Y-%m-%dT%H:%M:%S.%f')
+                }
+            )
+
+
+    return jsonify({'Machines' : all_instances})
 
 
 
